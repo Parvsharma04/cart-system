ced9d479aec986c21ad4be57c3c9f6ef53c62e54
Parv Sharma <sharmaparv.2004@gmail.com>
2024-08-21 23:33:16 +0530

    Add cart info

--- cart/cart.js
+++ cart/cart.js
@@ -42,24 +42,29 @@ function removeItem(e) {
 
 function renderData() {
   root.innerHTML = "";
+  bill.innerHTML = ""; // Clear previous total amount
   let products = getData();
 
   if (!products || products.length === 0) {
     let msg = document.createElement("h1");
     msg.innerText = "Cart Empty";
     const totalAmt = document.createElement('h1')
-    totalAmt.innerText = 'Total :  '
+    const totalItems= document.createElement('h1')
+    totalAmt.innerText = 'Net Amount :  '
     const price = document.createElement('span')
     price.innerText = '$0'
+    totalItems.innerText =  "Total Items :  0";
     totalAmt.append(price)
     bill.append(totalAmt)
+    bill.append(totalItems)
     root.append(msg);
   } else {
-    const totalAmt = document.createElement('h1')
-    totalAmt.innerText = 'Total :  '
-    const price = document.createElement('span')
+    const totalAmt = document.createElement('h1');
+    const totalItems= document.createElement('h1')
+    totalAmt.innerText = 'Net Amount :  ';
+    totalItems.innerText = `Total Items :  ${products.length}`;
+    const price = document.createElement('span');
     let totalPrice = 0;
-
     products.forEach((element) => {
       const div = document.createElement("div");
       const image = document.createElement("img");
@@ -81,7 +86,7 @@ function renderData() {
       price.innerHTML = "$ " + element.price;
       qty.innerText = "QTY : ";
       val.innerText = element.qty;
-      totalPrice += element.price*element.qty
+      totalPrice += element.price * element.qty;
       div.appendChild(image);
       div.appendChild(h1);
       div.appendChild(price);
@@ -92,10 +97,13 @@ function renderData() {
       div.appendChild(info);
       root.appendChild(div);
     });
-    price.innerText = "$"+totalPrice.toFixed(2);
-    totalAmt.append(price)
-    bill.append(totalAmt)
+
+    price.innerText = "$" + totalPrice.toFixed(2);
+    totalAmt.append(price);
+    bill.append(totalAmt);
+    bill.append(totalItems);
   }
 }
+
 
 renderData();
